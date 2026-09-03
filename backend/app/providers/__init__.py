fcc8ce4bfa1ba39eb704d1d7f7d446866b68b39f
ingbankingclient-ctrl <ing.banking.client@gmail.com>
2026-09-03 15:56:41 +0200

    Delete backend directory

--- backend/app/providers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .base import RacingProvider, ProviderError
-from .demo import DemoProvider
-from .pmu import PmuProvider
-from .turfbzh import TurfBzhProvider
-
-__all__ = ["RacingProvider", "ProviderError", "DemoProvider", "PmuProvider", "TurfBzhProvider"]
